9288854920fcc53aabc20238897e5449c6f25ce6
Jayson-PAmano <jaysonpa36@gmail.com>
2025-03-20 08:09:58 -0700

    JS Practice2

--- JS-Practice's/js/index1.js
+++ JS-Practice's/js/index1.js
@@ -1,8 +1,40 @@
-const x = 20;
-if (x >= 20) {
-  console.log("It is true");
-} else if (x > 22) {
-  console.log(x + "is greater that 22");
-} else {
-  console.log("Its not true");
+let fruits = ["apple", "orange", "grapes"];
+let colors = ["red", "green", "blue", "yellow"];
+
+const person = [
+  {
+    firstName: "John",
+    age: 35,
+    favColor: "blue,",
+  },
+  {
+    firstName: "Joe",
+    age: 33,
+    favColor: "red,",
+  },
+  {
+    firstName: "Jimmy",
+    age: 31,
+    favColor: "green,",
+  },
+];
+// let a = 0;
+// while (a < 5) {
+//    console.log(a);
+//   a++;
+// }
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+let i = 0;
+while (i < fruits.length) {
+  console.log("Index: " + i);
+  console.log("fruits: " + fruits[i]);
+  i++;
+}
+for (let i = 0; i < person.length; i++) {
+  console.log(person[i].firstName);
+}
+for (const color of colors) {
+  console.log(color);
 }
