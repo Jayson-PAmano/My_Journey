3014e77a7046225318e301775f4e280b2ca44005
Jayson-PAmano <jaysonpa36@gmail.com>
2025-06-17 00:32:49 -0700

    Traversy Media Variables and Constants

---
+++ TraversyMedia_JS/js/index.js
@@ -0,0 +1,24 @@
+let firstName = "John";
+let lastName = "Doe";
+let age = 30;
+console.log(firstName + lastName);
+
+score = 1;
+
+if (true) {
+  score = score + 1;
+}
+console.log(score);
+
+const arr = [1, 2, 3, 4];
+console.log(arr);
+arr.push(5);
+console.log(arr);
+
+const person = {
+  name: "Brad",
+};
+
+person.name = "John";
+person.age = 31;
+console.log(person.name, person.age);
