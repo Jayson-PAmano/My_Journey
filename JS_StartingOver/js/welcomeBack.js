b64eac5cbcdff07ba8792118982de1dbdf4813a2
Jayson-PAmano <jaysonpa36@gmail.com>
2025-07-02 22:12:31 -0700

    js practice

--- JS_StartingOver/js/welcomeBack.js
+++ JS_StartingOver/js/welcomeBack.js
@@ -1,6 +1,7 @@
 let fruits = ["apple", "orange", "grapes"];
 let colors = ["red", "green", "blue", "yellow"];
 let counter = 0;
+let counter2 = 5;
 let heroes = "Elemental Heroes";
 let letters = ["a", "b", "c"];
 let letters2 = ["d", "e", "f"];
@@ -54,13 +55,36 @@ getName2("Aster");
 
 getSum = (n1, n2) => n1 + n2;
 
-getSumOutput = (resuts) => {
-  resuts = getSum(5, 2);
-  console.log(`The sum is: ${resuts}`);
+getSumOutput = (a, b) => {
+  const result = getSum(a, b);
+  console.log(`The sum is: ${result}`);
+  return result;
 };
-getSumOutput();
-doubleOutput = (doubleResults) => {
-  doubleResults = getSumOutput() * 2;
+
+doubleOutput = (a, b) => {
+  const sum = getSumOutput(a, b);
+  const doubleResults = sum * 2;
   console.log(`The double is: ${doubleResults}`);
 };
-doubleOutput();
+doubleOutput(5, 2);
+
+//Loops
+
+while (counter <= 5) {
+  console.log(counter);
+  counter++;
+}
+console.log("\n");
+while (counter2 >= 0) {
+  console.log(counter2);
+  counter2--;
+}
+
+for (let i = 0; i < colors.length; i++) {
+  console.log(colors[i]);
+}
+console.log("Colors Reversed");
+
+for (let i = colors.length - 0; i >= 0; i--) {
+  console.log(colors[i]);
+}
